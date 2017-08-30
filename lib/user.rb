e330b2894bff32010404c9003f23fd3db5244c18
Brennan Brown <brennan.brown@edu.sait.ca>
2017-08-30 02:18:34 +0000

    Done.

---
+++ lib/user.rb
@@ -0,0 +1,9 @@
+                      # Inheritance in Ruby #
+# Working with a school domain model, the application has users that are either
+# teachers or students. Teachers and students share certain attributes and have
+# certain behaviors that are unique to them.
+
+class User
+  # Initializing the super class.
+  attr_accessor :first_name, :last_name
+end
