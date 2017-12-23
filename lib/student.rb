d0ac08ff05c1132fadef57e9a13702a9a5f49535
Gene Gi <junzheji@gmail.com>
2017-12-23 11:46:20 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,12 @@
-class Student 
+class Student < User
+  attr_accessor :knowledge
+  @knowledge = []
 
+  def learn(know)
+    @knowledge << know
+  end
+
+  def knowledge
+    @knowledge
+  end
 end
