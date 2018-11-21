24dcd3712e382f943779e68929f16b3907cb735c
Joseph Corona <josed.hc96@gmail.com>
2018-11-21 23:10:47 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,14 @@
-class Student 
+class Student < User
 
+  def initialize
+    @knowledge = []
+
+  end
+
+  attr_reader :knowledge
+
+  def learn(knowledge)
+    @knowledge << knowledge
+
+  end
 end
